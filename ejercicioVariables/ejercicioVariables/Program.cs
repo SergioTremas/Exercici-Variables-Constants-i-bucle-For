c84dc8485b386d62714d1dbba049276b8c744446
SergioTremas <43926519+SergioTremas@users.noreply.github.com>
2019-10-07 11:06:07 +0200

    Add files via upload

---
+++ ejercicioVariables/ejercicioVariables/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicioVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom, cognom1, cognom2;
+            int dia, any, mes;
+
+            nom = "Sergio";
+            cognom1 = "Tremosa";
+            cognom2 = "Jorrillo";
+            dia = 27;
+            any = 1983;
+            mes = 01;
+            string frase= "";
+            
+
+            Console.WriteLine(nom + " " + cognom1 + " " + cognom2 + " " + dia + "/" + mes + "/" + any);
+
+            const int traspas= 1948;
+            const int tiempo = 4;
+            int cantidad=0;
+            bool traspasNeix = false;
+
+            for(int x = traspas; x<= any; x = x+ tiempo)
+            {
+                cantidad++;
+
+                if(x== any)
+                {
+                    traspasNeix = true;
+
+                                 
+
+                }
+                
+            }
+
+            if (traspasNeix)
+            {
+                frase = "L'any de naixament es de traspas"; ;
+            }
+            else
+            {
+                frase = "L'any de naixament no es de traspas";
+            }
+
+
+            Console.WriteLine(frase);
+
+            Console.WriteLine("quantitat de anys de traspas " + cantidad);
+
+            Console.WriteLine("");
+
+            string nomSencer = nom + " "+ cognom1+ " " + cognom2;
+            string dataSecer = dia.ToString() + "/" + mes.ToString() + "/" + any.ToString();
+
+            Console.WriteLine("Mi nombre es " + nomSencer + "\n" + "Vaig néixer " + dataSecer + "\n" + frase);
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
